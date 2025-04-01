b7ca6d3543357cb4676788abf8c0b6a669e43622
Pasha Procenk <pashaprocenk@icloud.com>
2025-04-01 14:44:14 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Як умру, то поховайте\n" +
+                "Мене на могилі");
+        System.out.println("Серед степу широкого,\n" +
+                "На Вкраїні милій");
+
+        }
+    }
