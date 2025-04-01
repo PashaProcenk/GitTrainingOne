b55f76d7656e64d1f390ed4791f27675750ebe19
Pasha Procenk <pashaprocenk@icloud.com>
2025-04-01 22:11:41 +0300

    Second

---
+++ src/main2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class main2 {
+    public static void main(String[] args) {
+        System.out.println("Try to connect new classes");
+        Scanner scanner = new Scanner(System.in);
+        int git = scanner.nextInt();
+
+            if (git < 25) {
+                System.out.println("Git really noob");
+            } else {
+                System.out.println("This git no noob");
+
+            }
+        }
+    }
